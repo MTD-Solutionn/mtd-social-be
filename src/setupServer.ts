e86a8955ec98bac1dd0d300f170d51de2a210a39
Minh Nguyen <minh_nguyen@epam.com>
2023-02-08 21:55:11 +0700

    set up security, standard middlewares

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -7,6 +7,13 @@ import {
   NextFunction,
 } from 'express';
 import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
 console.log('src/setupServer.ts');
 export class ChattyServer {
   private app: Application;
@@ -20,8 +27,31 @@ export class ChattyServer {
     this.globalErrorHandler(this.app);
     this.startServer(this.app);
   }
-  private securityMiddleware(app: Application): void {}
-  private standardMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['test'],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      }),
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      }),
+    );
+  }
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
   private routesMiddleware(app: Application): void {}
   private globalErrorHandler(app: Application): void {}
   private startServer(app: Application): void {}
@@ -29,4 +59,3 @@ export class ChattyServer {
   private createSocketIO(httpServer: http.Server): void {}
   private startHttpServer(httpServer: http.Server): void {}
 }
-
